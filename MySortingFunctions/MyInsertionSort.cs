93c2404ad1ea7dc946edfedddd0b17ea547fe034
Justin <gallagherjust71@gmail.com>
2021-07-10 07:36:14 -0400

    Added name and date

--- MySortingFunctions/MyInsertionSort.cs
+++ MySortingFunctions/MyInsertionSort.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Justin Gallagher
+ * C# Practice
+ * 07/10/2021 */
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
--- MySortingFunctions/MySortingTests.cs
+++ MySortingFunctions/MySortingTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/* Justin Gallagher
+ * C# Practice
+ * 07/10/2021 */
+
+using System;
 
 namespace MySortingFunctions
 {
@@ -6,15 +10,19 @@ namespace MySortingFunctions
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Unsorted Array");
+
             //Create simple unsorted array
             int[] arr = {1, 5, 7, 3, 4, 9, 2, 6, 8};
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
+            Console.WriteLine("\n");
 
             //Run sorting algorithms on the array 
             MySelectionSort.SelectionSort(arr);
-            MyMergeSort.MergeSort(arr);
-            MyInsertionSort.InsertionSort(arr);
-            MyBubbleSort.BubbleSort(arr);
-            MyQuickSort.QuickSort(arr);
+            //MyMergeSort.MergeSort(arr);
+            //MyInsertionSort.InsertionSort(arr);
+            //MyBubbleSort.BubbleSort(arr);
+            //MyQuickSort.QuickSort(arr);
         }
     }
 }
