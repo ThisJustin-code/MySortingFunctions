1ac8349b720593a54cc2cee0ce942c8e1f2593d9
Justin <gallagherjust71@gmail.com>
2021-07-10 19:24:20 -0400

    Wrote bubble sort

--- MySortingFunctions/MyBubbleSort.cs
+++ MySortingFunctions/MyBubbleSort.cs
@@ -3,17 +3,35 @@
  * 07/10/2021 */
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MySortingFunctions
 {
     class MyBubbleSort
     {
-        //TODO: Write BubbleSort
-        public static int[] BubbleSort(int[] arr)
+        public static void BubbleSort(int[] arr)
         {
-            return arr;
+            int n = arr.Length - 1;
+            bool swaps = true;
+            while (swaps)
+            {
+                int count = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        count += 1;
+                    }
+                }
+                if (count == 0)
+                {
+                    swaps = false;
+                }
+            }
+            Console.WriteLine("Bubble Sort: ");
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
         }
     }
 }
