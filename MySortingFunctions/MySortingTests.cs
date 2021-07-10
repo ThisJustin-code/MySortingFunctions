4820684712312e60c337cc6e205142cf2800c033
Justin <gallagherjust71@gmail.com>
2021-07-10 19:24:43 -0400

    Added call to bubble sort

--- MySortingFunctions/MySortingTests.cs
+++ MySortingFunctions/MySortingTests.cs
@@ -13,15 +13,16 @@ namespace MySortingFunctions
             Console.WriteLine("Unsorted Array");
 
             //Create simple unsorted array
-            int[] arr = {1, 5, 7, 3, 4, 9, 2, 6, 8};
-            Console.WriteLine("[{0}]", string.Join(", ", arr));
+            int[] arr1 = {1, 5, 7, 3, 4, 9, 2, 6, 8};
+            int[] arr2 = { 1, 5, 7, 3, 4, 9, 2, 6, 8 };
+            Console.WriteLine("[{0}]", string.Join(", ", arr1));
             Console.WriteLine("\n");
 
             //Run sorting algorithms on the array 
-            MySelectionSort.SelectionSort(arr);
+            MySelectionSort.SelectionSort(arr1);
             //MyMergeSort.MergeSort(arr);
             //MyInsertionSort.InsertionSort(arr);
-            //MyBubbleSort.BubbleSort(arr);
+            MyBubbleSort.BubbleSort(arr2);
             //MyQuickSort.QuickSort(arr);
         }
     }
