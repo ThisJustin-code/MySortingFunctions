b0cf1ae659128774481c5474902c85ce673f12ad
Justin <gallagherjust71@gmail.com>
2021-07-10 07:35:06 -0400

    Wrote selection sort

--- MySortingFunctions/MySelectionSort.cs
+++ MySortingFunctions/MySelectionSort.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/* Justin Gallagher
+ * C# Practice
+ * 07/10/2021 */
+
+using System;
 
 namespace MySortingFunctions
 {
     class MySelectionSort
     {
-        //TODO: Write SelectionSort
-        public static int[] SelectionSort(int[] arr)
+        public static void SelectionSort(int[] arr)
         {
-            return arr;
+            int n = arr.Length - 1;
+            for (int i = 1; i < n; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j <= n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    int temp = arr[min];
+                    arr[min] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            Console.WriteLine("Selection Sort: ");
+            Console.WriteLine("[{0}]", string.Join(", ", arr));
         }
     }
 }
