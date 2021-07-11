a05432a43b650c6c2c86bb5dd7c60d557813a734
Justin <gallagherjust71@gmail.com>
2021-07-11 13:16:36 -0400

    Added call to insertion sort

--- MySortingFunctions/MySortingTests.cs
+++ MySortingFunctions/MySortingTests.cs
@@ -13,17 +13,20 @@ namespace MySortingFunctions
             Console.WriteLine("Unsorted Array");
 
             //Create simple unsorted array
-            int[] arr1 = {1, 5, 7, 3, 4, 9, 2, 6, 8};
+            int[] arr1 = { 1, 5, 7, 3, 4, 9, 2, 6, 8 };
             int[] arr2 = { 1, 5, 7, 3, 4, 9, 2, 6, 8 };
+            int[] arr3 = { 1, 5, 7, 3, 4, 9, 2, 6, 8 };
+            int[] arr4 = { 1, 5, 7, 3, 4, 9, 2, 6, 8 };
+            int[] arr5 = { 1, 5, 7, 3, 4, 9, 2, 6, 8 };
             Console.WriteLine("[{0}]", string.Join(", ", arr1));
             Console.WriteLine("\n");
 
             //Run sorting algorithms on the array 
             MySelectionSort.SelectionSort(arr1);
-            //MyMergeSort.MergeSort(arr);
-            //MyInsertionSort.InsertionSort(arr);
-            MyBubbleSort.BubbleSort(arr2);
-            //MyQuickSort.QuickSort(arr);
+            MyInsertionSort.InsertionSort(arr2);
+            MyBubbleSort.BubbleSort(arr3);
+            //MyMergeSort.MergeSort(arr4);
+            //MyQuickSort.QuickSort(arr5);
         }
     }
 }
