bd114d115a8f1ff9b68151ab41bcb0ae1c40f172
Justin <gallagherjust71@gmail.com>
2021-07-11 14:15:17 -0400

    Wrote quick sort with partition method

--- MySortingFunctions/MyQuickSort.cs
+++ MySortingFunctions/MyQuickSort.cs
@@ -11,9 +11,37 @@ namespace MySortingFunctions
     class MyQuickSort
     {
         //TODO: Write QuickSort
-        public static int[] QuickSort(int[] arr)
+        public static void QuickSort(int[] arr, int low, int high)
         {
-            return arr;
+            if (low < high)
+            {
+                int mid = Partition(arr, low, high);
+
+                QuickSort(arr, low, mid - 1);
+                QuickSort(arr, mid + 1, high);
+            }
+        }
+
+        private static int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j <= high - 1; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    int t2 = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t2;
+                }
+            }
+            int t1 = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = t1;
+
+            return i + 1;
         }
     }
 }
