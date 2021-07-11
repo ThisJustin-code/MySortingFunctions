648c5d73fd625882d9a94ce71c23841d3f19574c
Justin <gallagherjust71@gmail.com>
2021-07-11 19:27:28 -0400

    Wrote merge sort with merge method

--- MySortingFunctions/MyMergeSort.cs
+++ MySortingFunctions/MyMergeSort.cs
@@ -2,18 +2,70 @@
  * C# Practice
  * 07/10/2021 */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace MySortingFunctions
 {
     class MyMergeSort
     {
-        //TODO: Write MergeSort
-        public static int[] MergeSort(int[] arr)
+        public static void MergeSort(int[] arr, int low, int high)
         {
-            return arr;
+            if (low < high)
+            {
+                int mid = (low + high - 1) / 2;
+                MergeSort(arr, low, mid);
+                MergeSort(arr, mid + 1, high);
+                Merge(arr, low, mid, high);
+            }
+        }
+
+        private static void Merge(int[] arr, int low, int mid, int high)
+        {
+            int n1 = mid - low + 1;
+            int n2 = high - mid;
+            
+            int[] lowArr = new int[n1];
+            int[] highArr = new int[n2];
+
+            for (int i = 0; i < n1; ++i)
+            {
+                lowArr[i] = arr[low + i];
+            }
+            for (int j = 0; j < n2; ++j)
+            {
+                highArr[j] = arr[mid + 1 + j];
+            }
+
+            //p and q represent initial indices for lowArr and highArr, respectively.
+            //r is the initial index of the merged sub-array
+            int p = 0, q = 0, r = low;
+
+            while (p < n1 && q < n2)
+            {
+                if (lowArr[p] <= highArr[q])
+                {
+                    arr[r] = lowArr[p];
+                    p++;
+                }
+                else
+                {
+                    arr[r] = highArr[q];
+                    q++;
+                }
+                r++;
+            }
+
+            while (p < n1)
+            {
+                arr[r] = lowArr[p];
+                p++;
+                r++;
+            }
+
+            while (q < n2)
+            {
+                arr[r] = highArr[q];
+                q++;
+                r++;
+            }
         }
     }
 }
